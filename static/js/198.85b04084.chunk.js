995f6f977bb54457fa2d09b6025737b4840cbfa8
sambensim <sambensim@gmail.com>
2024-07-26 09:55:46 -0700

    Updates

---
+++ static/js/198.85b04084.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvite_test=self.webpackChunkvite_test||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.85b04084.chunk.js.map
