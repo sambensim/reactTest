bae3929e1c3eabe9b40d61b4ea407e719ea2a180
sambensim <sambensim@gmail.com>
2024-07-26 09:55:18 -0700

    Removed unused import

--- src/components/DataList.jsx
+++ src/components/DataList.jsx
@@ -1,6 +1,6 @@
 // import React from 'react';
 import PropTypes from 'prop-types';
-import DataForm from './DataForm';
+// import DataForm from './DataForm';
 
 const DataList = ({dataList}) => {
     return (
